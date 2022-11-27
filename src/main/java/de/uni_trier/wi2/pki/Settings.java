9d632e9e9d66d5ac9d3822fd48c9dad68a8f1db9
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2022-11-28 00:22:25 +0100

    Update 28.11.2022 #1
    -changed Settings.java

--- src/main/java/de/uni_trier/wi2/pki/Settings.java
+++ src/main/java/de/uni_trier/wi2/pki/Settings.java
@@ -2,14 +2,14 @@ package de.uni_trier.wi2.pki;
 
 public class Settings {
 
-    static int labelIndex = 20;                  //Das Ergebnis dieser Spalte soll prognostiziert werden
+    static int labelIndex = 0;                  //Das Ergebnis dieser Spalte soll prognostiziert werden
 
-    static String sourcePath = "src/main/resources/Weather.csv"; //Ort der zu untersuchenden CSV-Datei
-    static String xmlPath = "src/main/resources/xml/1Test.xml";     //Ort der generierten XML-Datei
+    static String sourcePath = "src/main/resources/diabetes_binary_5050split_health_indicators_BRFSS2015-smaller.csv"; //Ort der zu untersuchenden CSV-Datei
+    static String xmlPath = "src/main/resources/xml/test1.xml";     //Ort der generierten XML-Datei
 
     static boolean ignoreHead = true;          //Dieser Wert soll true sein, wenn in der ersten Zeile die Attributbezeichnungen stehen
-    static String delimiter = ";";             //Das Trennzeichen in der CSV-Datei
-    static boolean testIfDiscrete = true;      //true: Potenzielle bereits diskrete Attribute werden erfasst (Es wird erfasst, ob in einer Spalte nur ganze Zahlen stehen), false: Es wird nur auf kategorische und kontinuierliche Werte getestet
+    static String delimiter = ",";             //Das Trennzeichen in der CSV-Datei
+    static boolean testIfDiscrete = false;      //true: Potenzielle bereits diskrete Attribute werden erfasst (Es wird erfasst, ob in einer Spalte nur ganze Zahlen stehen), false: Es wird nur auf kategorische und kontinuierliche Werte getestet
 
     static int numberOfBins = 5;                //Anzahl der Bins, es wird ab 1 gezählt
     static boolean binningProcedure = true;     //true: Gleiche Intervallgröße, false: gleiche Punkteanzahl pro Intervall
